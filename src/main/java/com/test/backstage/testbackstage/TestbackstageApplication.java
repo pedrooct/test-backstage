8b36dfa40d336159458f1468423727bfa3f4238f
Pedro Costa <pedro.costa15@vodafone.com>
2025-02-17 19:03:17 +0000

    First commit

---
+++ src/main/java/com/test/backstage/testbackstage/TestbackstageApplication.java
@@ -0,0 +1,13 @@
+package com.test.backstage.testbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbackstageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/backstage/testbackstage/TestbackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.backstage.testbackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbackstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
